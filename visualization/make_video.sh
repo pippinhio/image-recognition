4b1388425a6e9e8aea647414904c6fae349dfbb9
Franz Hoffmann <fhoffma@tulane.edu>
2020-10-31 11:00:42 +0100

    clean up video stuff

--- visualization/make_video.sh
+++ visualization/make_video.sh
@@ -4,11 +4,14 @@
 rm -rf video
 mkdir video
 
-cd pics
+cd jpg
 for file in *.jpg; do
   convert $file -crop 1500x800+400+1 ../video/$file
 done
 cd ..
 
 cd video
-ffmpeg -framerate 1 -i %04d.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p network_visualization.mp4
+ffmpeg -framerate 1 -i %04d.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p ../network_visualization.mp4
+cd ..
+
+rm -rf video
