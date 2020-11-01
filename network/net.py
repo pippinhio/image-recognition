2d743d568b8df13745eb81474a7ab2745bdd1211
Franz Hoffmann <fhoffma@tulane.edu>
2020-11-01 13:33:53 +0100

    remove dead code

--- network/net.py
+++ network/net.py
@@ -13,15 +13,6 @@ class Net(nn.Module):
     self.lin1  = nn.Linear(in_features=400, out_features=120)
     self.lin2  = nn.Linear(in_features=120, out_features= 84)
     self.lin3  = nn.Linear(in_features= 84, out_features=  2)
-  
-#  def __init__(self):
-#    super(Net, self).__init__()
-#    self.conv1 = nn.Conv2d(3, 6, 5)
-#    self.pool = nn.MaxPool2d(2, 2)
-#    self.conv2 = nn.Conv2d(6, 16, 5)
-#    self.fc1 = nn.Linear(16 * 5 * 5, 120)
-#    self.fc2 = nn.Linear(120, 84)
-#    self.fc3 = nn.Linear(84, 2)
 
   def forward(self, x):
     x = F.relu(self.conv1(x))
