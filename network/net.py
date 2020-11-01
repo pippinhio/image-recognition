18e1f5c4f63aeb53d3cce81242f96ea255321f34
Franz Hoffmann <fhoffma@tulane.edu>
2020-11-01 17:04:53 +0100

    smaller net

--- network/net.py
+++ network/net.py
@@ -1,26 +1,23 @@
 # from https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html#sphx-glr-beginner-blitz-neural-networks-tutorial-py
 
 import torch.nn as nn
-import torch.nn.functional as F
 
 class Net(nn.Module):
   def __init__(self):
     super(Net, self).__init__()
-    self.conv1 = nn.Conv2d(kernel_size=5, in_channels=3, out_channels= 6)
+    self.conv1 = nn.Conv2d(kernel_size=5, in_channels=3, out_channels=3)
     self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-    self.conv2 = nn.Conv2d(kernel_size=5, in_channels=6, out_channels=16)
+    self.conv2 = nn.Conv2d(kernel_size=5, in_channels=3, out_channels=3)
     self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-    self.lin1  = nn.Linear(in_features=400, out_features=120)
-    self.lin2  = nn.Linear(in_features=120, out_features= 84)
-    self.lin3  = nn.Linear(in_features= 84, out_features=  2)
+    self.lin1  = nn.Linear(in_features=75, out_features=10)
+    self.lin2  = nn.Linear(in_features=10, out_features= 2)
 
   def forward(self, x):
-    x = F.relu(self.conv1(x))
+    x = self.conv1(x)
     x = self.pool1(x)
-    x = F.relu(self.conv2(x))
+    x = self.conv2(x)
     x = self.pool2(x)
-    x = x.view(-1, 400)
-    x = F.relu(self.lin1(x))
-    x = F.relu(self.lin2(x))
-    x = self.lin3(x)
+    x = x.view(-1, 75)
+    x = self.lin1(x)
+    x = self.lin2(x)
     return x
