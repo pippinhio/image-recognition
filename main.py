858547753aa98a6a6380fc296ab8aed1e7902fc8
Franz Hoffmann <fhoffma@tulane.edu>
2020-11-13 00:34:30 +0100

    allow for interactive mode

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+#to read https://towardsdatascience.com/understanding-pytorch-with-an-example-a-step-by-step-tutorial-81fc5f8c4e8e
+
 #Run with e.g. ./main.py franz nina
 
+import code
 import sys
 import warnings
 
@@ -13,14 +16,14 @@ from network.net import Net
 from network.train import train
 from network.validate import predict, validate
 
-def main():
-  categories = sys.argv[1:]
-  if len(categories) == 0:
-    categories = ["franz", "nina"]
+def main(categories=('franz', 'nina')):
   training_set, validation_set = load(categories)
   net = Net()
   train(net, training_set)
-  validate(net, validation_set)
+  accuracy = validate(net, validation_set)
+  
+  return accuracy
+#  code.interact(local=locals())
 
 if __name__ == '__main__':
   warnings.filterwarnings("ignore")
@@ -28,8 +31,6 @@ if __name__ == '__main__':
   torch.manual_seed(0)
   np.random.seed(0)
   
-#  import code
-#  code.interact(local=locals())
-  main()
-
-
+  if len(sys.argv) > 1 and sys.argv[1] == "interact":
+    code.interact(local=locals())
+  main(sys.argv[1:])
